e569272397601e5b388b9c7f104c8aaab90267c1
raulm <raulm@DESKTOP-AUJH7VA>
2021-12-13 22:59:29 +0000

    Kata4 versión 2

--- src/kata4/main/Kata4.java
+++ src/kata4/main/Kata4.java
@@ -10,15 +10,24 @@ import kata4.view.MailHistogramBuilder;
 import kata4.view.MailListReader;
 
 public class Kata4 {
+    
 
     public static void main(String[] args) throws FileNotFoundException {
-        
         String nameFile = new String("email.txt");
-        List<Mail> mailList = MailListReader.read(nameFile);
-        Histogram<String> histogram = MailHistogramBuilder.build(mailList);
-        
-        HistogramDisplay hist = new HistogramDisplay(histogram);
-        hist.execute();
+        input(nameFile);
     }
+    
+    public static void input(String nameFile) throws FileNotFoundException{
+        process(MailListReader.read(nameFile));
+    }
+    
+    public static void process(List<Mail> mailList){
+        output(MailHistogramBuilder.build(mailList));
+    }
+    
+    public static void output(Histogram<String> histogram){
+        new HistogramDisplay(histogram).execute();
+    }
+    
     
 }
